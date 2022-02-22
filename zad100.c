bf35ac95a93330e7b1393425d192a5b02bb8fa88
fiki2k <91737444+fiki2k@users.noreply.github.com>
2022-02-22 11:23:57 +0100

    Add files via upload

---
+++ zad100.c
@@ -0,0 +1,19 @@
+// Porgram treba generirat random polje.
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+   int velicina;
+   printf("Unesi velicinu polja:");
+   scanf("%d",&velicina);
+   int polje[velicina],i;
+   for(i=0;i<velicina;i++)
+     polje[i]=rand()%100;   //Generira random broj od 0 do 99
+   printf("\nElementi polja su:");
+   for(i=0;i<velicina;i++)
+   {
+     printf("\nBroj elementa %d:%d",i+1,polje[i]);
+   }
+   return 0;
+}
